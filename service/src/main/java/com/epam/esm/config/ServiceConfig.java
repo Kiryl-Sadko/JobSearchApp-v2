d8d681a5ace1b69852ef480505a3a0cf4018c0cd
Kiryl Sadko <kirill.beltech@gmail.com>
2021-03-27 22:35:01 +0300

    service module has initialized

---
+++ service/src/main/java/com/epam/esm/config/ServiceConfig.java
@@ -0,0 +1,13 @@
+package com.epam.esm.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan(basePackages = "com.epam.esm")
+@Import(PersistenceConfig.class)
+@EnableTransactionManagement
+public class ServiceConfig {
+}
