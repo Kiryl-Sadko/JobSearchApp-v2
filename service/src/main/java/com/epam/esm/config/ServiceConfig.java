bde6f723454e74cdaca2674a5ac0a3d12ba94dd4
Kiryl Sadko <kirill.beltech@gmail.com>
2021-03-28 21:05:13 +0300

    ServiceCongig class has created

--- service/src/main/java/com/epam/esm/config/ServiceConfig.java
+++ service/src/main/java/com/epam/esm/config/ServiceConfig.java
@@ -1,11 +1,11 @@
 package com.epam.esm.config;
 
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@Configuration
+@SpringBootConfiguration
 @ComponentScan(basePackages = "com.epam.esm")
 @Import(PersistenceConfig.class)
 @EnableTransactionManagement
