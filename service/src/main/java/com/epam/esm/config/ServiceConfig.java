06247f9b2b352f539a2a362fcf52c8477bcdb820
Kiryl Sadko <kirill.beltech@gmail.com>
2021-04-22 22:45:19 +0300

    validator bean has added to application context

--- service/src/main/java/com/epam/esm/config/ServiceConfig.java
+++ service/src/main/java/com/epam/esm/config/ServiceConfig.java
@@ -1,13 +1,22 @@
 package com.epam.esm.config;
 
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
 
 @SpringBootConfiguration
 @ComponentScan(basePackages = "com.epam.esm")
 @Import(PersistenceConfig.class)
 @EnableTransactionManagement
 public class ServiceConfig {
+
+    @Bean
+    public Validator validator() {
+        return Validation.buildDefaultValidatorFactory().getValidator();
+    }
 }
