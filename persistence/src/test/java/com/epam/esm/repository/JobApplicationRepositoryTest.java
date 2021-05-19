3c480bf0e365cd0b40d39794e532b38f90fd3551
Kiryl Sadko <kirill.beltech@gmail.com>
2021-05-19 22:44:25 +0300

    tests of the persistence layer have been fixed

--- persistence/src/test/java/com/epam/esm/repository/JobApplicationRepositoryTest.java
+++ persistence/src/test/java/com/epam/esm/repository/JobApplicationRepositoryTest.java
@@ -2,6 +2,8 @@ package com.epam.esm.repository;
 
 import com.epam.esm.config.PersistenceConfig;
 import com.epam.esm.entity.JobApplication;
+import com.epam.esm.entity.User;
+import com.epam.esm.entity.Vacancy;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,9 +12,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.*;
 
 @SpringBootTest(classes = PersistenceConfig.class)
@@ -24,6 +28,30 @@ public class JobApplicationRepositoryTest {
 
     @Autowired
     private JobApplicationRepository repository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private VacancyRepository vacancyRepository;
+
+    @Test
+    public void shouldSaveJobApplication() {
+        User user = userRepository.findTopByOrderByIdDesc();
+        Vacancy vacancy = vacancyRepository.findTopByOrderByIdDesc();
+
+        JobApplication jobApplication = new JobApplication();
+        jobApplication.setSalary(BigDecimal.valueOf(100));
+        jobApplication.setUser(user);
+        jobApplication.setVacancy(vacancy);
+        jobApplication.setResponseDate(LocalDateTime.now());
+
+        List<JobApplication> before = repository.findAll();
+        repository.save(jobApplication);
+
+        List<JobApplication> after = repository.findAll();
+        assertTrue(before.size() < after.size());
+    }
 
     @Test
     public void shouldFindAllJobApplications() {
