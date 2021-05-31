f02bbbf74ba3c53c03d4da8667205be494d8b59c
Kiryl Sadko <kirill.beltech@gmail.com>
2021-05-31 23:28:53 +0300

    Tests chenged to ignore auth

--- web/src/test/java/com/epam/esm/controller/UserControllerTest.java
+++ web/src/test/java/com/epam/esm/controller/UserControllerTest.java
@@ -3,6 +3,7 @@ package com.epam.esm.controller;
 import com.epam.esm.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         @Sql(executionPhase = BEFORE_TEST_METHOD, scripts = {"classpath:scheme.sql", "classpath:init.sql"}),
         @Sql(executionPhase = AFTER_TEST_METHOD, scripts = "classpath:drop.sql")
 })
+@AutoConfigureMockMvc(addFilters = false)
 public class UserControllerTest {
 
     @Autowired
