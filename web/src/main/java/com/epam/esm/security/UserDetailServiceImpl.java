eb92fb694a921c7851fcd2bcbe44d1560ff9c1e3
Kiryl Sadko <kirill.beltech@gmail.com>
2021-05-31 23:22:05 +0300

    UserDeatailsService has been implemented

---
+++ web/src/main/java/com/epam/esm/security/UserDetailServiceImpl.java
@@ -0,0 +1,25 @@
+package com.epam.esm.security;
+
+import com.epam.esm.entity.User;
+import com.epam.esm.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("UserDetailServiceImpl")
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByName(username).orElseThrow(() ->
+                new UsernameNotFoundException("User doesn't exist!"));
+        return UserDetailsImpl.fromUser(user);
+    }
+}
